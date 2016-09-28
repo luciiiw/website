94c6aee9a761a65714330f9a665f139471a81c5a
katrinacrisostomo <katcri@gmail.com>
2016-09-28 13:21:44 -0400

    added stuffs

--- site.js
+++ site.js
@@ -1,6 +1,8 @@
 $( document ).ready(function() {
   function triangle() {
-    $('#section-triangle').css("border-left", $( window ).width() + "px solid transparent" );
+    $('#section-triangle').css("border-left", $( window ).width() + "px solid transparent");
+    $('#section-triangle2').css("border-left", $(window).width() + "px solid white");
+    $('#section-triangle3').css("border-left", $(window).width() + "px solid white");
   }
   triangle();
 
