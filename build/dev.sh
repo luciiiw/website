9b41a8847e6cfceeb5bb5da719b526df6e224860
Bryan Cho <bryanc208@gmail.com>
2016-12-28 10:19:26 -0800

    Stopped removing typing file for firebase module

--- build/dev.sh
+++ build/dev.sh
@@ -3,7 +3,6 @@
 npm install -g angular-cli
 npm install -g firebase-tools
 npm install firebase
-rm node_modules/firebase/firebase.d.ts
 echo "export const SECRETS = { FIREBASE_API_KEY: '$FIREBASE_DEV_API_KEY' }" > src/configs/secrets.ts
 ng build
 firebase deploy -P $FIREBASE_DEV_PROJECT_ID --token=$FIREBASE_TOKEN --non-interactive
