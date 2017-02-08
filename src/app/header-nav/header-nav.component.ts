575c62a9ec680823b73bd539b4ac471f9058ed17
Aaron Te <aaronjte@gmail.com>
2017-02-08 01:07:32 -0500

    Switch `header-nav` links to hamburger menu takeover in mobile - Not the cleanest code, but it will do for now

--- src/app/header-nav/header-nav.component.ts
+++ src/app/header-nav/header-nav.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { Router } from '@angular/router';
 
 const LANDING_VIEW_URL = '/';
@@ -9,16 +9,56 @@ const LANDING_VIEW_URL = '/';
   styleUrls: ['./header-nav.component.css']
 })
 
-export class HeaderNavComponent implements OnInit {
+export class HeaderNavComponent implements AfterViewInit {
+  links: Object[];
   isLandingView: boolean;
 
   constructor(router: Router) {
     router.events.subscribe(event => {
       this.isLandingView = LANDING_VIEW_URL === event.url;
     });
+
+    this.links = [
+      {
+        name: 'Projects',
+        link: '/projects'
+      },
+      {
+        name: 'Non Profits',
+        link: '/nonprofits'
+      },
+      {
+        name: 'Students',
+        link: '/students'
+      },
+      {
+        name: 'Team',
+        link: '/team'
+      }
+    ];
   }
 
-  ngOnInit() {
-  }
+  ngAfterViewInit() {
+    document.getElementById('hamburgler').addEventListener('click', checkNav);
+    $('.hamburgler-menu-list a').on('click', () => closeNav());
+    window.addEventListener('keyup', function (e) {
+      if (e.keyCode == 27) closeNav();
+    }, false);
 
+    function checkNav() {
+      if (document.body.classList.contains('hamburgler-active')) {
+        closeNav();
+      } else {
+        openNav();
+      }
+    }
+
+    function closeNav() {
+      document.body.classList.remove('hamburgler-active');
+    }
+
+    function openNav() {
+      document.body.classList.add('hamburgler-active');
+    }
+  }
 }
